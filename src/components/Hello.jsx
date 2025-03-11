65e225a2f85c5e1405db3cb967cdc302132929bf
SridharSolaris <21A91A05I2@aec.edu.in>
2025-03-11 18:58:02 +0530

    updated

--- src/components/Hello.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Hello() {
-  return <div>Hello wrold</div>;
-}
-
-export default Hello;
---
+++ src/pages/Test.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import axios from "axios";
+
+const Test = () => {
+
+    const fetchData = async () => {
+        try {
+            const response = await axios.post(
+                `http://localhost:4000/api/apps/${APP_ID}/validate-ip`,
+                { ip: "127.0.0.1" },
+                {
+                    headers: {
+                        "Content-Type": "application/json",
+                        "x-api-key": API_KEY,
+                        "x-app-id": APP_ID,
+                        "x-sdk-request": "true",
+                    }
+                }
+            );
+
+            // Handle response data
+            console.log("Data fetched:", response.data);
+        } catch (error) {
+            console.error("Error fetching data:", error);
+        }
+    };
+
+    return (
+        <div>
+            <button onClick={fetchData} className="mt-4 px-4 py-2 bg-blue-500 text-white rounded">
+                Fetch Data
+            </button>
+        </div>
+    )
+}
+
+export default Test
