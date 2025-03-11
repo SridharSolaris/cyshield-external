c29502b6a2b614a58053e44d3cdfe9d538b4f51a
SridharSolaris <21A91A05I2@aec.edu.in>
2025-03-11 18:59:21 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React, { useEffect } from "react";
 import { setConfig, ProtectedPage, setupAxiosInterceptors } from "firewalljk31-sdk"; // Ensure correct import path
-import Hello from "./components/Hello";
+import Hello from "./pages/Home";
 import Navbar from "./Navbar";
 import Login from './pages/Login';
 import Signup from './pages/Signup';
+import Test from "./pages/Test";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import axios from "axios"; // Use the default Axios instance
 
@@ -29,27 +30,7 @@ function App() {
   }, [APP_ID, API_KEY]);
 
   // Function to make an API request
-  const fetchData = async () => {
-    try {
-      const response = await axios.post(
-        `http://localhost:4000/api/apps/${APP_ID}/validate-ip`,
-        { ip: "127.0.0.1" },
-        {
-          headers: {
-            "Content-Type": "application/json",
-            "x-api-key": API_KEY,
-            "x-app-id": APP_ID,
-            "x-sdk-request": "true",
-          }
-        }
-      );
 
-      // Handle response data
-      console.log("Data fetched:", response.data);
-    } catch (error) {
-      console.error("Error fetching data:", error);
-    }
-  };
 
   return (
     <div className="App">
@@ -60,14 +41,13 @@ function App() {
             <Routes>
               <Route path="/home" element={<Hello />} />
               <Route path="/login" element={<Login />} />
+              <Route path="/test-api" element={<Test />} />
               <Route path="/signup" element={<Signup />} />
               <Route path="*" element={<Login />} />
             </Routes>
             <h1>Protected Content</h1>
             <p>This content is protected by the firewall SDK.</p>
-            <button onClick={fetchData} className="mt-4 px-4 py-2 bg-blue-500 text-white rounded">
-              Fetch Data
-            </button>
+
           </div>
         </ProtectedPage>
       </BrowserRouter>
